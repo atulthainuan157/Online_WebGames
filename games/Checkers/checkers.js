f381afc609b870c1ecbff7a09dbf94f122064678
atulthainuan157 <atulthainuan157@gmail.com>
2026-06-20 01:26:48 +0530

    feat: initialize Tic-Tac-Toe HTML structure and add Checkers game styles and basic piece initialization logic

--- games/Checkers/checkers.js
+++ games/Checkers/checkers.js
@@ -1,6 +1,13 @@
 let white = document.querySelectorAll(".box");
 
-for(let i=0; i<10; i++) {
-    white[i].innerHTML = "Hii";
-    white[i].style.color= "red"
+for (let i = 0; i <= 64;) {
+    white[i].innerHTML = "O";
+    white[i].style.color = "red"
+    white[i].style.fontSize = "50px"
+    if (i % 2 == 0) {
+        i++;
+    }
+    else {
+        i = i + 7;
+    }
 }
